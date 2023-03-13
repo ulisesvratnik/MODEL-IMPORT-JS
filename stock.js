ed0e4b797e2c5b3a8993d8205a5092fed943e129
ulisesvratnik <ulisesvratnik01@outlook.com>
2023-03-13 19:02:19 -0300

    Subiendo tercera entrega JS

---
+++ stock.js
@@ -0,0 +1,8 @@
+let stockProductos = [
+    {id: 1, nombre: "Smartwacht Y13", color: "DORADO", precio: 12.999, precioAnterior: 15.999, img: '/imagenes/Y13 GOLD.PNG', cantidad: 1,},
+    {id: 2, nombre: "Smartwacht Y13", color: "NEGRO", precio: 12.999, precioAnterior: 15.999, img: '/imagenes/y13black.PNG', cantidad: 1,},
+    {id: 3, nombre: "Smartwacht Y13", color: "ROSA", precio: 12.999, precioAnterior: 15.999, img: '/imagenes/Y13 PINK.PNG', cantidad: 1,},
+    {id: 4, nombre: "Smartwacht T55", color: "BLANCO", precio: 12.999, precioAnterior: 15.999, img: '/imagenes/T55 BLANCO.PNG', cantidad: 1,},
+    {id: 5, nombre: "Smartwacht T55", color: "NEGRO", precio: 12.999, precioAnterior: 15.999, img: '/imagenes/T55 NEGRO.PNG', cantidad: 1,},
+    {id: 6, nombre: "Smartwacht T55", color: "ROSA", precio: 12.999, precioAnterior: 15.999, img: '/imagenes/T55 ROSA.PNG', cantidad: 1,},
+]
